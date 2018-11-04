17dd1a8a65c0d2a09f57f11d6570ea309c00d163
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-11-03 18:11:28 -0700

    Updates

---
+++ precache-manifest.fa82922c9b8cd124095fce431e22ed66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3877f9b99f4c8ba8e8c7",
+    "url": "/quilt-react2/static/js/runtime~main.3877f9b9.js"
+  },
+  {
+    "revision": "b3c3cfb256b18fd76c74",
+    "url": "/quilt-react2/static/js/main.b3c3cfb2.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/quilt-react2/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "b3c3cfb256b18fd76c74",
+    "url": "/quilt-react2/static/css/main.a47c1ef2.chunk.css"
+  },
+  {
+    "revision": "e06db0e8f1360686700b236474f24f9a",
+    "url": "/quilt-react2/index.html"
+  }
+];
