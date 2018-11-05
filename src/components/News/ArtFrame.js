a0bee7f0ffc608965ca0c47063f5fb44274e4f4f
David De Wulf <contact.dewulf@gmail.com>
2018-11-05 15:16:58 -0800

    Add Political Color & Image

--- src/components/News/ArtFrame.js
+++ src/components/News/ArtFrame.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import NewsImg from '../../img/news.jpeg';
 
 const Card = styled.div`
   display: block;
@@ -8,16 +9,15 @@ const Card = styled.div`
   position: relative;
   margin: 5px;
   border-radius: 3px;
-  background-color: blue;
   background-image: url(${props => props.img});
   background-position: center;
   background-size: cover;
   transition: all 300ms;
   :hover {
     transform: scale(1.05);
-    transition: all 300ms;
+    transition: all ease-in-out 300ms;
   }
-  transition: all 300ms;
+  transition: all ease-in-out 300ms;
 
   a {
     color: white;
@@ -31,6 +31,13 @@ const Card = styled.div`
 
   button {
     position: absolute;
+    bottom: 0px;
+    right: 0px;
+    font-size: 1.5em;
+    background-color: #c8498b;
+    padding-left: 12px;
+    border: 1px solid #c8498b;
+    border-radius: 3px 0px 0px 0px;
   }
 
   .source {
@@ -41,27 +48,49 @@ const Card = styled.div`
 `;
 
 const P = styled.p`
-  background-color: ${props => props.color};
+  background-color: ${props =>
+    props.color === 'REPLUBLICAN'
+      ? '#ff000080'
+      : props.color === 'DEMOCRATIC'
+        ? '#0000ff80'
+        : '#27b06580'};
   text-align: left;
   padding: 30px 10px;
   height: 100%;
   line-height: 2em;
-  text-align: center;
   border-radius: 3px;
+  transition: all ease-in-out 300ms;
+  :hover {
+    background-color: #000000ad;
+    transition: all ease-in-out 300ms;
+  }
+
+  span {
+    background-color: ${props =>
+      props.color === 'REPLUBLICAN'
+        ? 'red'
+        : props.color === 'DEMOCRATIC'
+          ? 'blue'
+          : 'green'};
+  }
 `;
 
 const ArtFrame = props => {
   const { item, savedItem } = props;
+  // If there isn't image
+  if (item.urlToImage === null) item.urlToImage = NewsImg;
   return (
-    <Card img={item.urlToImage} color="blue">
+    <Card img={item.urlToImage}>
       <a href={item.url} target="">
-        <P color="#ff009996">
+        <P color={item.party}>
+          <span color={item.party} className="source">
+            {item.source.name}
+          </span>{' '}
+          <br />
           {item.title} <br />
-          <span className="source">{item.source.name}</span> <br />
-          {item.publishedAt} <br />
         </P>
       </a>
-      {/* <button onClick={() => savedItem(item)}>Save Article</button> */}
+      <button onClick={() => savedItem(item)}>💾</button>
     </Card>
   );
 };
