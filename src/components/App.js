c9b0c892464adf9a6847ffef9c46dbf03d3172dc
Jamarr Edwards <jamarredwards@gmail.com>
2018-11-05 18:21:45 -0800

    updated branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+<<<<<<< HEAD
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import News from './News/News';
+=======
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import News from '../components/News/News';
+import Dashboard from './Dashboard';
+>>>>>>> 55cfbd669ff4d0af77a48d55a5226634d250d5f6
 import Navbar from './Navbar';
 import Footer from './footer';
 
-
-
-
-
-
 class App extends Component {
-
   render() {
     return (
       <BrowserRouter className="App">
@@ -19,6 +19,7 @@ class App extends Component {
           <Navbar />
           <Switch>
             <Route path="/" exact component={News} />
+            <Route path="/dashboard" exact component={Dashboard} />
           </Switch>
           <Footer />
         </>
@@ -26,6 +27,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
--- src/components/News/ArtFrame.js
+++ src/components/News/ArtFrame.js
@@ -1,28 +1,97 @@
 import React from 'react';
+import styled from 'styled-components';
+import NewsImg from '../../img/news.jpeg';
+
+const Card = styled.div`
+  display: block;
+  width: 300px;
+  height: 300px;
+  position: relative;
+  margin: 5px;
+  border-radius: 3px;
+  background-image: url(${props => props.img});
+  background-position: center;
+  background-size: cover;
+  transition: all 300ms;
+  :hover {
+    transform: scale(1.05);
+    transition: all ease-in-out 300ms;
+  }
+  transition: all ease-in-out 300ms;
+
+  a {
+    color: white;
+    color: white;
+    font-weight: 600;
+    font-size: 1.2rem;
+    :hover {
+      text-decoration: none;
+    }
+  }
+
+  button {
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+    font-size: 1.5em;
+    background-color: #c8498b;
+    padding-left: 12px;
+    border: 1px solid #c8498b;
+    border-radius: 3px 0px 0px 0px;
+  }
+
+  .source {
+    padding: 5px;
+    border: 1px solid white;
+    border-radius: 3px;
+  }
+`;
+
+const P = styled.p`
+  background-color: ${props =>
+    props.color === 'REPLUBLICAN'
+      ? '#ff000080'
+      : props.color === 'DEMOCRATIC'
+        ? '#0000ff80'
+        : '#27b06580'};
+  text-align: left;
+  padding: 30px 10px;
+  height: 100%;
+  line-height: 2em;
+  border-radius: 3px;
+  transition: all ease-in-out 300ms;
+  :hover {
+    background-color: #000000ad;
+    transition: all ease-in-out 300ms;
+  }
+
+  span {
+    background-color: ${props =>
+      props.color === 'REPLUBLICAN'
+        ? 'red'
+        : props.color === 'DEMOCRATIC'
+          ? 'blue'
+          : 'green'};
+  }
+`;
 
 const ArtFrame = props => {
   const { item, savedItem } = props;
+  // If there isn't image
+  if (item.urlToImage === null) item.urlToImage = NewsImg;
   return (
-    <div>
+    <Card img={item.urlToImage}>
       <a href={item.url} target="">
-        <div className="card-image">
-          <img src={item.urlToImage} alt="description" />
-        </div>
-        <div className="article-text text-left">
-          <span style={{ fontSize: '22px' }} className="row">
-            {item.title}
-          </span>
-          <span className="row">
-            <p className="sourceDisplay">{item.source.name}</p>
-          </span>
-          <span className="row">{item.author}</span>
-          <span className="row">{item.publishedAt}</span>
-        </div>
+        <P color={item.party}>
+          <span color={item.party} className="source">
+            {item.source.name}
+          </span>{' '}
+          <br />
+          {item.title} <br />
+        </P>
       </a>
-      <button className="row" onClick={() => savedItem(item)}>
-        <i className="fa fa-save" /> Save Article
-      </button>
-    </div>
+      <button onClick={() => savedItem(item)}>💾</button>
+    </Card>
   );
 };
 
