4b96c2f1189788653844231cf5b5ec9d20aa1b5a
David De Wulf <contact.dewulf@gmail.com>
2018-11-05 16:05:12 -0800

    Fix Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import News from './News/News';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Navbar from './Navbar';
 import Footer from './footer';
 
-
-
-
-
-
 class App extends Component {
-
   render() {
     return (
       <BrowserRouter className="App">
         <>
           <Navbar />
           <Switch>
-            <Route path="/Dashboard" exact={true} component={Dashboard} />
-            <Route path="/" exact component={News} />
+            <Route path="/" exact />
+            <Route path="/Dashboard" component={Dashboard} />
           </Switch>
           <Footer />
         </>
@@ -28,6 +21,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
