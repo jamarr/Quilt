8548aead655b412480a3768eda1924f195dbfa21
David De Wulf <contact.dewulf@gmail.com>
2018-11-05 16:05:12 -0800

    Fix Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import News from './News/News';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Navbar from './Navbar';
 import Footer from './footer';
 
-
-
-
-
-
 class App extends Component {
-  // constructor() {
-  //   super()
-  // }
-  // handleLogIn = () => { //Make into Single Function On Navbar 
-  // Use Teriniary to determine Login = True or false 
-
-  // }
   render() {
     return (
       <BrowserRouter className="App">
         <>
           <Navbar />
           <Switch>
-            <Route path="/Dashboard" exact={true} component={Dashboard} />
-            <Route path="/" exact component={News} />
+            <Route path="/" exact />
+            <Route path="/Dashboard" component={Dashboard} />
           </Switch>
           <Footer />
         </>
@@ -34,6 +21,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
