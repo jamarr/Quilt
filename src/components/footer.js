b0fa9e2bd612a8a3ea4edac4e65247239f400dae
David De Wulf <contact.dewulf@gmail.com>
2018-11-05 16:30:36 -0800

    Fix ClassName error

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Title = styled.h1`
-font-size: 35px;
-padding: 20px;
-`
-
-
-
-
-
+  font-size: 35px;
+  padding: 20px;
+`;
 
 export default class Footer extends Component {
   render() {
     return (
-       <div>
+      <div>
         <footer className="footer">
-        <div className="container">
-        <Title>STAY CONNECTED</Title>
-        <div className= "row">
-        <ul className="nav flex-column flex-sm-row align-self-center">
-            <li className=" col nav-item"><i className="fa fa-facebook-f fa-2x"></i></li>
-            <li className=" col nav-item"><i className="fa fa-twitter-square fa-2x"></i></li>
-            <li className=" col nav-item"><i className="fa fa-linkedin fa-2x" ></i></li>
-            <li className=" col nav-item"><i className="fa fa-github-square fa-2x"></i></li>
-        </ul>
-        </div>
-        <Title>&copy; 2018 QUILT</Title>
-        </div>
-        <div className="row d-flex align-content-between flex-wrap">
-        <form method="POST" action="https://formspree.io/YOUREMAILHERE">
-            <div class="form-group row">
-            <label for="inputEmail3">Email</label>
+          <div className="container">
+            <Title>STAY CONNECTED</Title>
+            <div className="row">
+              <ul className="nav flex-column flex-sm-row align-self-center">
+                <li className=" col nav-item">
+                  <i className="fa fa-facebook-f fa-2x" />
+                </li>
+                <li className=" col nav-item">
+                  <i className="fa fa-twitter-square fa-2x" />
+                </li>
+                <li className=" col nav-item">
+                  <i className="fa fa-linkedin fa-2x" />
+                </li>
+                <li className=" col nav-item">
+                  <i className="fa fa-github-square fa-2x" />
+                </li>
+              </ul>
             </div>
-            <div className="form-group">
-            <input type="text" name="name"/>
-            </div>
-            <div className="form-group">
-            <input type="email" name="_replyto"/>
-            </div>
-            <div className="form-group">
-            <input type="submit" value="Send"/>
-            </div>
-        </form>
-        </div>
+            <Title>&copy; 2018 QUILT</Title>
+          </div>
+          <div className="row d-flex align-content-between flex-wrap">
+            <form method="POST" action="https://formspree.io/YOUREMAILHERE">
+              <div className="form-group row">
+                <label for="inputEmail3">Email</label>
+              </div>
+              <div className="form-group">
+                <input type="text" name="name" />
+              </div>
+              <div className="form-group">
+                <input type="email" name="_replyto" />
+              </div>
+              <div className="form-group">
+                <input type="submit" value="Send" />
+              </div>
+            </form>
+          </div>
         </footer>
       </div>
-    )
+    );
   }
 }
