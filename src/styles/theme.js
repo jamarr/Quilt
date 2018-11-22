c36c9ac70e41af8c2446e60af50072425667f78c
David De Wulf <contact.dewulf@gmail.com>
2018-11-21 19:43:58 -1000

    Add Colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,7 +2,15 @@ const theme = {
   colors: {
     primary: '#fcf0eb', // light pink
     secondary: '#c8498b', //  Dark pink
-    text: 'white'
+    text: 'white',
+    republican: 'red',
+    democratic: 'blue',
+    others: 'green'
+  },
+  display: {
+    mobile: '425px',
+    tablet: '768px',
+    desktop: '1024px'
   }
 };
 
