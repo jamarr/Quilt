2a4ca7832d731be6118ff0538f7e6321d4c2ed84
David De Wulf <contact.dewulf@gmail.com>
2018-11-21 19:40:19 -1000

    Refactor Button Component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import theme from '../../styles/theme';
 
-const Btn = styled.button`
+const Button = styled.button`
   background: #c8498b;
   padding: 10px;
   border-radius: 3px;
   border: none;
-  color: ${theme.colors.text};
+  color: ${props => props.theme.colors.text};
   font-weight: 600;
   :hover {
     opacity: 0.8;
   }
 `;
 
-export default ({ children }) => <Btn>{children}</Btn>;
+export default ({ children }) => <Button>{children}</Button>;
