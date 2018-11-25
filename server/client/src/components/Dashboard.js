ca37c14585c5410e9fefb34230a0a10e8c971875
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-11-24 19:43:56 -0800

    fullstack

---
+++ server/client/src/components/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+
+
+class Dashboard extends React.Component {
+
+
+
+  render() {
+    return (
+
+      <h2> This Is our Dashboard  </h2>
+
+    )
+  }
+}
+
+export default Dashboard
