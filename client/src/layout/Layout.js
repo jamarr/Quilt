b6ba0f8e0f2cd8375f65f7959623fadcd01dba17
David De Wulf <contact.dewulf@gmail.com>
2018-11-26 17:09:01 -0800

    Add DashButton Component

--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import News from '../components/News/News';
-import Dashboard from '../components/Dashboard';
+import Dashboard from '../components/Dashboard/Dashboard';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Error from './Error';
@@ -10,8 +10,8 @@ const Layout = () => (
   <>
     <Navbar />
     <Switch>
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/" component={News} exact />
+      <Route path='/dashboard' component={Dashboard} />
+      <Route path='/' component={News} exact />
       <Route component={Error} />
     </Switch>
     <Footer />
