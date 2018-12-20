3d6fe89f4a47bf3dc8b006244f212db02f0dd657
David De Wulf <contact.dewulf@gmail.com>
2018-12-19 17:35:44 -0800

    Set Up dotenv

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
+require('dotenv').config();
+require('./models/User');
+require('./services/passport');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser');
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
-
+const config = require('./config');
+const path = require('path');
 const app = express();
 
-app.use(bodyParser.json());
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    config.mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Connected to Mango'));
+
+app.use(express.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
+    keys: [config.cookieKey]
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
@@ -32,11 +36,11 @@ if (process.env.NODE_ENV === 'production') {
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(config.port, () =>
+  console.log(`Express server listening on localhost:${config.port}`)
+);
